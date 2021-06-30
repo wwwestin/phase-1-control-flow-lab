0ab2f711346e63a33ce1d7fa2733bc108721ca58
Mohammed Shaikh <46161085+mqshaikh8@users.noreply.github.com>
2021-06-30 18:24:54 -0400

    commit tree 7a8fb1308f17d77057cd0d538b868ae6880a2661

--- index.js
+++ index.js
@@ -1,11 +1,38 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(distance){
   // Write your code here!
+  if( distance <= 400){
+    return 'This one is on me!'
+  }
+  else if (distance > 2000 && distance < 2500){
+    return 'I will gladly take your thirty bucks.'
+  }
+  else {
+    return 'No can do.'
+  }
+  
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(city){
   // Write your code here!
+  return city === 'NYC' ?  "Ok, sounds good." : "No go."
+  // if(city === 'NYC'){
+  //   return "Ok, sounds good."
+  // }
+  // else {
+  //   return "No go."
+  // }
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(tip){
   // Write your code here!
+  if(tip === 'generous'){
+    return  "Thank you so much."
+  }
+  else if (tip === 'not as generous'){
+    return "Thank you."
+  }
+  else {
+    return 'Bye.'
+  }
+  
 }
